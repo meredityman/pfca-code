55166f07597e1d78605673eeb03677803dbace9e
Meredith Thomas <meredityman@gmail.com>
2023-03-28 19:59:58 +0200

    Before breakshoe

--- week-3/storygenerator.py
+++ week-3/storygenerator.py
@@ -1,41 +1,34 @@
 import random
 
 
+firstNames = [
+    "Xriii",    # 0
+    "Griggle",  # 1
+    "Sldfy",    # 2
+    "Wifrt"     # 3
+]
+
+secondNames = [
+    "Troooble",
+    "Scrungee",
+    "Leeber",
+    "Fromw"
+]
+
+
+def getRandomListEntry(myList):
+    numEntries = len(myList)
+    randomIndex = random.randrange(0, numEntries)
+    return myList[randomIndex]
+
 def main():
-
-    firstNames = [
-        "Xriii",    # 0
-        "Griggle",  # 1
-        "Sldfy",    # 2
-        "Wifrt"     # 3
-    ]
-
-
-    secondNames = [
-        "Troooble",
-        "Scrungee",
-        "Leeber",
-        "Fromw"
-    ]
-
     # Get random first name
-    firstNameNum = len(firstNames)
-    firstNameIndex = random.randrange(0,firstNameNum)
-    randomFirstName = firstNames[firstNameIndex]
-    print(firstNameNum, firstNameIndex, randomFirstName)
-
-
-    # Get random second name
-    secondNameNum = len(secondNames)  
-    secondNameIndex = random.randrange(0,secondNameNum)
-    randomSecondName = secondNames[secondNameIndex]
-    print(secondNameNum, secondNameIndex, randomSecondName)
-
+    randomFirstName = getRandomListEntry(firstNames)
+    randomSecondName = getRandomListEntry(secondNames)
 
     fullRandomName = randomFirstName + " " + randomSecondName
     print(fullRandomName)
 
 
 if __name__ == "__main__":
-    print("heelo")
     main() 
